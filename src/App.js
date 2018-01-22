322e7cc6854830e595988d9ef4602acd2b319100
itReverie <brenda@itreverie.com>
2018-01-22 14:40:12 -0500

    Easy start to add a rechart graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line , XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+
+const data=[
+  {"name": "Anna", "uv":18000},
+  {"name": "Luis", "uv":19000},
+  {"name": "Sandra", "uv":20000},
+  {"name": "James", "uv":21000},
+  {"name": "John", "uv":22000}
+];
 
 class App extends Component {
   render() {
@@ -11,6 +20,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React and Recharts</h1>
         </header>
 
+        <LineChart width={500} height={300} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
 
         </div>
     );
